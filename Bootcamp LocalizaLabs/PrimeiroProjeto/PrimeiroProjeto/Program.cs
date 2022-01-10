d6dffcb843786e6f8498ba131779a399eb9fae06
Jeferson Sabino dos Santos Junior <86162489+JefersonTaiyou@users.noreply.github.com>
2022-01-09 22:24:15 -0300

    Create a first project C#
    Criando o Primeiro Projeto C# do módulo "Introdução ao C# e .NET"

---
+++ Bootcamp LocalizaLabs/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    public class CalculadoraBasica
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        public int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "World";
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ Bootcamp LocalizaLabs/PrimeiroProjeto/PrimeiroProjetoTests/CalculadoraBasicaTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeiroProjeto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiroProjeto.Tests
+{
+    [TestClass()]
+    public class CalculadoraBasicaTests
+    {
+        [TestMethod()]
+        public void SomarTest()
+        {
+            var a = 10;
+            var b = 20;
+            var esperado = 30;
+            var calcular = new CalculadoraBasica();
+
+            var resultado = calcular.Somar(a, b);
+
+            Assert.AreEqual(esperado,resultado);
+        }
+
+        [TestMethod()]
+        public void SubtrairTest()
+        {
+            var a = 10;
+            var b = 5;
+            var esperado = 5;
+            var calcular = new CalculadoraBasica();
+
+            var resultado = calcular.Subtrair(a, b);
+
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void MultiplicarTest()
+        {
+            var a = 10;
+            var b = 20;
+            var esperado = 200;
+            var calcular = new CalculadoraBasica();
+
+            var resultado = calcular.Multiplicar(a, b);
+
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void DividirTest()
+        {
+            var a = 10;
+            var b = 5;
+            var esperado = 2;
+            var calcular = new CalculadoraBasica();
+
+            var resultado = calcular.Dividir(a, b);
+
+            Assert.AreEqual(esperado, resultado);
+        }
+    }
+}
