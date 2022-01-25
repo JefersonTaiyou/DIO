d057eadba405c651949954658b1ae4d613a4a72d
Jeferson Sabino dos Santos Junior <86162489+JefersonTaiyou@users.noreply.github.com>
2022-01-25 15:48:42 -0300

    Update Ref.cs

--- Bootcamp-LocalizaLabs/PrimeiroProjeto/PrimeiroProjeto/Ref.cs
+++ Bootcamp-LocalizaLabs/PrimeiroProjeto/PrimeiroProjeto/Ref.cs
@@ -16,4 +16,20 @@ namespace PrimeiroProjeto
             System.Console.WriteLine($"{i} {j}");    // Escreve "2 1"
         }
     }
+    
+    public class Out
+    {
+        static void Dividir(int x, int y, out int resultado, out int resto) 
+        {
+            resultado = x / y;
+            resto = x % y;
+        }
+        
+        public static void Dividir() 
+        {
+            Dividir(10, 3, out int resultado, out int resto);
+            System.Console.WriteLine("{0} {1}", resultado, resto);	// Escreve "3 1"
+        }
+    }
+    
 }
